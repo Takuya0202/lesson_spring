cb5d1f4d8dbc0c5d76fe7279b6da4f20f62f5676
takuya0202 <shimozono.t.24kdgn@gmail.com>
2026-03-31 16:02:44 +0900

    add lombok

---
+++ src/lombok/lesson_spring/src/main/java/com/example/lesson_spring/User.java
@@ -0,0 +1,11 @@
+package com.example.lesson_spring;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class User {
+    private String name;
+    private int age;
+}
---
+++ src/lombok/lesson_spring/src/test/java/com/example/lesson_spring/LessonSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lesson_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
