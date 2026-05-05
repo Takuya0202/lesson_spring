c3654d2901baab3c843aac469220f383aba1edea
takuya0202 <shimozono.t.24kdgn@gmail.com>
2026-05-05 23:38:28 +0900

    add injenction project

---
+++ src/injenction-sample/src/test/java/com/example/injenction_sample/InjenctionSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.injenction_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjenctionSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
