cbd935c3c03db2c893166fa56c4148a1f77612f2
takuya0202 <shimozono.t.24kdgn@gmail.com>
2026-05-30 23:58:38 +0900

    complete requrest param

--- src/requestparam/src/main/java/com/example/requestparam/controller/RequestParamController.java
+++ src/requestparam/src/main/java/com/example/requestparam/controller/RequestParamController.java
@@ -1,13 +1,29 @@
 package com.example.requestparam.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RequestParamController {
     @GetMapping("show")
     public String showView() {
         return "input";
+    }
+
+    @GetMapping("result")
+    public String showResultGet(@RequestParam String val,
+            Model model) {
+        model.addAttribute("value", val);
+        return "output";
+    }
+
+    @PostMapping("result")
+    public String showResultPost(@RequestParam String val, Model model) {
+        model.addAttribute("value", val);
+        return "output";
     }
 
 }
