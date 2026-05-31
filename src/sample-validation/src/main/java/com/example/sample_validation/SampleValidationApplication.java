96a2899f5a09f5bafb08b70eb16336daefe08550
takuya0202 <shimozono.t.24kdgn@gmail.com>
2026-05-31 22:38:52 +0900

    add validation project

---
+++ src/sample-validation/src/main/java/com/example/sample_validation/SampleValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.sample_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleValidationApplication.class, args);
+	}
+
+}
