42e49bde2edc038f653ef647f1efa775f815175e
takuya0202 <shimozono.t.24kdgn@gmail.com>
2026-05-31 18:59:31 +0900

    add

---
+++ src/path-valiable-sample/src/main/java/com/example/path_valiable_sample/PathValiableSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.path_valiable_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathValiableSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathValiableSampleApplication.class, args);
+	}
+
+}
