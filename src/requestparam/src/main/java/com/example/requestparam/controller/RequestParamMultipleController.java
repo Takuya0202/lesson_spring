50cbdadd0dd6d39e4cce6ae01ada1030b833a429
takuya0202 <shimozono.t.24kdgn@gmail.com>
2026-05-31 18:52:28 +0900

    add form class

--- src/requestparam/src/main/java/com/example/requestparam/controller/RequestParamMultipleController.java
+++ src/requestparam/src/main/java/com/example/requestparam/controller/RequestParamMultipleController.java
@@ -1,13 +1,10 @@
 package com.example.requestparam.controller;
 
-import java.time.LocalDate;
-
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.requestparam.form.SampleForm;
 
 @Controller
 public class RequestParamMultipleController {
@@ -16,15 +13,23 @@ public class RequestParamMultipleController {
         return "entry";
     }
 
+    // @PostMapping("confirm")
+    // public String confirmView(
+    // @RequestParam String name,
+    // @RequestParam Integer age,
+    // @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam LocalDate birth,
+    // Model model) {
+    // model.addAttribute("name", name);
+    // model.addAttribute("age", age);
+    // model.addAttribute("birth", birth);
+    // return "confirm";
+    // }
+
     @PostMapping("confirm")
-    public String confirmView(
-            @RequestParam String name,
-            @RequestParam Integer age,
-            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam LocalDate birth,
-            Model model) {
-        model.addAttribute("name", name);
-        model.addAttribute("age", age);
-        model.addAttribute("birth", birth);
-        return "confirm";
+    public String confirmView(SampleForm f) {
+        // TODO: process POST request
+
+        return "confirm2";
     }
+
 }
