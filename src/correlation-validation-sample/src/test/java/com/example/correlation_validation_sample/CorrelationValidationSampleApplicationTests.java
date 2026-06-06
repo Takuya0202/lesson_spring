6b3bf051b88a2fac7ae3e5971ba09d9d51aa175c
takuya0202 <shimozono.t.24kdgn@gmail.com>
2026-06-06 22:15:03 +0900

    add

---
+++ src/correlation-validation-sample/src/test/java/com/example/correlation_validation_sample/CorrelationValidationSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.correlation_validation_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorrelationValidationSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
