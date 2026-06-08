92d5daf22047093d4257d1f7347ed414b04e9e39
takuya0202 <shimozono.t.24kdgn@gmail.com>
2026-06-08 23:45:31 +0900

    add mybatis sample

---
+++ src/mybatis/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into books(title , author ) values ('title1' , 'aaa');
+insert into books(title , author ) values ('title2' , 'bbb');
+insert into books(title , author ) values ('title3' , 'ccc');
---
+++ src/mybatis/src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+create table books (
+  id int primary key auto_increment,
+  title varchar(255) not null,
+  author varchar(255) not null
+);
